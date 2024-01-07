d3cbf3a6d185d21c5f99c16cf44ef816af4aaa96
bhardwajvinay004 <bhardwajvinay004@gmail.com>
2024-01-07 14:49:49 +0530

    organized code and updated index.html

--- stacked-bar-chart/index.js
+++ stacked-bar-chart/index.js
@@ -8,172 +8,215 @@ const margin = {
   right: 20,
   bottom: 50,
 };
-
 const chartWidth = width - margin.left - margin.right;
 const chartHeight = height - margin.top - margin.bottom;
-
 const colors = ["#fc0072", "#71e95e", "#ffd05b", "#4d69fe", "#ef7434"];
-const lineColor = "#3a91bf";
 
-const categories = d3.keys(data);
+let categories;
+let groups;
+let groupData;
+let stackedData;
 
-const groups = d3
-  .keys(data[categories[0]])
-  .filter((g) => g.indexOf("Group ") !== -1);
+let chartDiv;
+let svg;
 
-const groupData = categories.map((key) => {
-  return {
-    quarter: key,
-    ...data[key],
-  };
-});
+let barColors;
+let legends;
+let tooltip;
 
-const barColors = d3.scaleOrdinal().domain(groups).range(colors);
-const stackedData = d3.stack().keys(groups)(groupData);
+let xAxisScale;
+let yAxisScale;
 
-const chartDiv = d3.select("#stacked-bar-chart");
+function createChart() {
+  organizeJsonData();
 
-const svg = chartDiv
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  createChartSvg();
+  createStackedBarColorsScale();
+  createLegends();
+  createTooltip();
 
-const legends = svg
-  .selectAll(".legend")
-  .data(groups)
-  .enter()
-  .append("g")
-  .classed("legend", true)
-  .attr("transform", "translate(-60,-60)");
+  createXaxisScale();
+  createYaxisScale();
 
-legends
-  .append("rect")
-  .attr("x", (d, i) => {
-    return width / 4 + i * 110;
-  })
-  .attr("y", 0)
-  .attr("width", 18)
-  .attr("height", 18)
-  .attr("fill", (d, i) => colors[i]);
+  createStackedBarChart();
+  createRevenueGrowthLineChart();
+  createLineChartDots();
+}
 
-legends
-  .append("text")
-  .attr("x", (d, i) => {
-    return 30 + width / 4 + i * 110;
-  })
-  .attr("y", 12)
-  .attr("dy", ".18em")
-  .text((d) => d);
+function organizeJsonData() {
+  categories = d3.keys(data);
+  groups = d3
+    .keys(data[categories[0]])
+    .filter((g) => g.indexOf("Group ") !== -1);
+  groupData = categories.map((key) => {
+    return {
+      quarter: key,
+      ...data[key],
+    };
+  });
 
-const tooltip = d3
-  .select("body")
-  .append("div")
-  .attr("id", "tooltip")
-  .style("opacity", 0);
+  stackedData = d3.stack().keys(groups)(groupData);
+}
 
-const xBarChart = d3
-  .scaleBand()
-  .domain(categories)
-  .range([0, chartWidth])
-  .padding([0.6]);
+function createChartSvg() {
+  chartDiv = d3.select("#stacked-bar-chart");
+  svg = chartDiv
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+}
 
-svg
-  .append("g")
-  .attr("transform", "translate(0," + chartHeight + ")")
-  .call(d3.axisBottom(xBarChart).tickSize(0).tickPadding(15));
+function createStackedBarColorsScale() {
+  barColors = d3.scaleOrdinal().domain(groups).range(colors);
+}
 
-const yBarChart = d3
-  .scaleLinear()
-  .domain([
-    0,
-    d3.max(groupData, (d) => {
-      const tmpObj = { ...d };
-      delete tmpObj.quarter;
-      delete tmpObj["Revenue Growth"];
-      return d3.sum(d3.values(tmpObj));
-    }),
-  ])
-  .range([chartHeight, 0]);
+function createLegends() {
+  legends = svg
+    .selectAll(".legend")
+    .data(groups)
+    .enter()
+    .append("g")
+    .classed("legend", true)
+    .attr("transform", "translate(-60,-60)");
 
-svg
-  .append("g")
-  .call(
-    d3
-      .axisLeft(yBarChart)
-      .tickSize(0)
-      .tickPadding(15)
-      .tickFormat(d3.format("$.2s"))
-  );
+  legends
+    .append("rect")
+    .attr("x", (d, i) => {
+      return width / 4 + i * 110;
+    })
+    .attr("y", 0)
+    .attr("width", 18)
+    .attr("height", 18)
+    .attr("fill", (d, i) => colors[i]);
 
-svg
-  .append("g")
-  .selectAll("g")
-  .data(stackedData)
-  .enter()
-  .append("g")
-  .attr("fill", (d) => {
-    return barColors(d.key);
-  })
-  .selectAll("rect")
-  .data((d) => {
-    return d;
-  })
-  .enter()
-  .append("rect")
-  .attr("x", (d) => {
-    return xBarChart(d.data.quarter);
-  })
-  .attr("y", (d) => {
-    return yBarChart(d[1]);
-  })
-  .attr("height", (d) => {
-    return yBarChart(d[0]) - yBarChart(d[1]);
-  })
-  .attr("width", xBarChart.bandwidth())
-  .on("mousemove", (e, d) => onBarMouseMove(e, d))
-  .on("mouseout", (e) => onMouseOut(e));
+  legends
+    .append("text")
+    .attr("x", (d, i) => {
+      return 30 + width / 4 + i * 110;
+    })
+    .attr("y", 12)
+    .attr("dy", ".18em")
+    .text((d) => d);
+}
 
-svg
-  .append("path")
-  .datum(groupData)
-  .attr("fill", "none")
-  .attr("stroke", lineColor)
-  .attr("stroke-width", 2)
-  .style("z-index", 999)
-  .attr(
-    "d",
-    d3
-      .line()
-      .x(function (d) {
-        return xBarChart(d.quarter) + xBarChart.bandwidth() / 2;
-      })
-      .y(function (d) {
-        return yBarChart(d["Revenue Growth"]);
-      })
-  );
+function createTooltip() {
+  tooltip = d3
+    .select("body")
+    .append("div")
+    .attr("id", "tooltip")
+    .style("opacity", 0);
+}
 
-svg
-  .selectAll(".dot")
-  .data(groupData)
-  .enter()
-  .append("circle")
-  .classed("dot", true)
-  .attr("cx", function (d) {
-    return xBarChart(d.quarter) + xBarChart.bandwidth() / 2;
-  })
-  .attr("cy", function (d) {
-    return yBarChart(d["Revenue Growth"]);
-  })
-  .on("mousemove", (e, d) => onDotMouseMove(e, d))
-  .on("mouseout", (e) => onMouseOut(e));
+function createXaxisScale() {
+  xAxisScale = d3
+    .scaleBand()
+    .domain(categories)
+    .range([0, chartWidth])
+    .padding([0.6]);
+
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + chartHeight + ")")
+    .call(d3.axisBottom(xAxisScale).tickSize(0).tickPadding(15));
+}
+
+function createYaxisScale() {
+  yAxisScale = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(groupData, (d) => {
+        const tmpObj = { ...d };
+        delete tmpObj.quarter;
+        delete tmpObj["Revenue Growth"];
+        return d3.sum(d3.values(tmpObj));
+      }),
+    ])
+    .range([chartHeight, 0]);
+
+  svg
+    .append("g")
+    .call(
+      d3
+        .axisLeft(yAxisScale)
+        .tickSize(0)
+        .tickPadding(15)
+        .tickFormat(d3.format("$.2s"))
+    );
+}
+
+function createStackedBarChart() {
+  svg
+    .append("g")
+    .selectAll("g")
+    .data(stackedData)
+    .enter()
+    .append("g")
+    .attr("fill", (d) => {
+      return barColors(d.key);
+    })
+    .selectAll("rect")
+    .data((d) => {
+      return d;
+    })
+    .enter()
+    .append("rect")
+    .attr("x", (d) => {
+      return xAxisScale(d.data.quarter);
+    })
+    .attr("y", (d) => {
+      return yAxisScale(d[1]);
+    })
+    .attr("height", (d) => {
+      return yAxisScale(d[0]) - yAxisScale(d[1]);
+    })
+    .attr("width", xAxisScale.bandwidth())
+    .on("mousemove", (e, d) => onBarMouseMove(e, d))
+    .on("mouseout", (e) => onMouseOut(e));
+}
+
+function createRevenueGrowthLineChart() {
+  svg
+    .append("path")
+    .datum(groupData)
+    .classed("line", true)
+    .attr(
+      "d",
+      d3
+        .line()
+        .x(function (d) {
+          return xAxisScale(d.quarter) + xAxisScale.bandwidth() / 2;
+        })
+        .y(function (d) {
+          return yAxisScale(d["Revenue Growth"]);
+        })
+    );
+}
+
+function createLineChartDots() {
+  svg
+    .selectAll(".dot")
+    .data(groupData)
+    .enter()
+    .append("circle")
+    .classed("dot", true)
+    .attr("cx", function (d) {
+      return xAxisScale(d.quarter) + xAxisScale.bandwidth() / 2;
+    })
+    .attr("cy", function (d) {
+      return yAxisScale(d["Revenue Growth"]);
+    })
+    .on("mousemove", (e, d) => onDotMouseMove(e, d))
+    .on("mouseout", (e) => onMouseOut(e));
+}
 
 function onBarMouseMove(e, d) {
-  d3.select(e.target).transition().duration(200).style("opacity", 0.4);
+  d3.select(e.target).transition().duration(100).style("opacity", 0.4);
 
   const f = d3.format("$.2s");
-  tooltip.transition().duration(200).style("opacity", 1);
+  tooltip.transition().duration(100).style("opacity", 1);
   tooltip
     .html(`${f(d[1] - d[0])}`)
     .style("left", `${e.layerX}px`)
@@ -181,16 +224,18 @@ function onBarMouseMove(e, d) {
 }
 
 function onMouseOut(e) {
-  d3.select(e.target).transition().duration(200).style("opacity", 1);
+  d3.select(e.target).transition().duration(100).style("opacity", 1);
 
-  tooltip.transition().duration(200).style("opacity", 0);
+  tooltip.transition().duration(100).style("opacity", 0);
 }
 
 function onDotMouseMove(e, d) {
   const f = d3.format("$.2s");
-  tooltip.transition().duration(200).style("opacity", 1);
+  tooltip.transition().duration(100).style("opacity", 1);
   tooltip
     .html(`${f(d["Revenue Growth"])}`)
     .style("left", `${e.layerX}px`)
     .style("top", `${e.layerY + 10}px`);
 }
+
+createChart();
